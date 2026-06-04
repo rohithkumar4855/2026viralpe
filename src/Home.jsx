548762ee341f2a1a5332386587a9e951e7b649a4
Rohit kumar <devallakumar4855@gmail.com>
2026-06-04 22:26:46 +0530

    fixed issues in Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,11 @@
 import Heropage from '../src/components/home/Hero';
-import GrowingTogether from '../src/components/home/earningcard';
-import PopularVouchers from '../src/components/home/vouchers';
-import Customersupport from '../src/components/home/customersupport';
-import PromotionalCards from '../src/components/home/promotionalcards';
+import GrowingTogether from '../src/components/home/EarningCard';
+import PopularVouchers from '../src/components/home/Vouchers';
+import Customersupport from '../src/components/home/Customersupport';
+import PromotionalCards from '../src/components/home/Promotionalcards';
 import WhyChooseViralPe from '../src/components/home/Whychoose';
-import ReferAndEarn from '../src/components/home/referandearn';
-import SupportBanner from '../src/components/home/supportbanner';
+import ReferAndEarn from '../src/components/home/Referandearn';
+import SupportBanner from '../src/components/home/Supportbanner';
 import FAQSection from '../src/components/home/Faqquestion';
 import ContactUs from '../src/components/home/Contactus';
 
