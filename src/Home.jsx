6fdc6a5dfbcac96d6be0ac7432dad52373594fad
Rohit kumar <devallakumar4855@gmail.com>
2026-06-05 10:10:07 +0530

    solved vercel deploy issues

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,10 +2,10 @@ import Heropage from '../src/components/home/Hero';
 import GrowingTogether from '../src/components/home/EarningCard';
 import PopularVouchers from '../src/components/home/Vouchers';
 import Customersupport from '../src/components/home/Customersupport';
-import PromotionalCards from '../src/components/home/Promotionalcards';
+import PromotionalCards from '../src/components/home/PromotionalCards';
 import WhyChooseViralPe from '../src/components/home/Whychoose';
-import ReferAndEarn from '../src/components/home/Referandearn';
-import SupportBanner from '../src/components/home/Supportbanner';
+import ReferAndEarn from '../src/components/home/ReferandEarn';
+import SupportBanner from '../src/components/home/SupportBanner';
 import FAQSection from '../src/components/home/Faqquestion';
 import ContactUs from '../src/components/home/Contactus';
 
